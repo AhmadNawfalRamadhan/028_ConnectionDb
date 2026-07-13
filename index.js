cbcf10d38a3dfefb94dec28634a9e283ce560f27
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-07-13 15:28:36 +0700

    setup express and postgres connection for biodata

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const {Pool} = require ('pg');
+
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mahasiswa',
+    password: '12345678',
+    port: 5432,
+});
+
+app.use(express.json());
+
+app.get('/biodata', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM biodata');
+
+        res.status(200).json({
+            message: 'Berhasil mengambil data biodata',
+            data: result.rows
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: 'Terjadi kesalahan pada server atau database' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`);
+});
